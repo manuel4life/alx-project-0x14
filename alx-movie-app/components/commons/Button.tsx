fe33032b53d745d8452115d5b0cc2efd0afa2b3e
manuel4life <allotey4eva@gmail.com>
2025-08-10 20:33:15 +0200

    components

---
+++ alx-movie-app/components/commons/Button.tsx
@@ -0,0 +1,9 @@
+const Button: React.FC = () => {
+  return (
+    <div>
+      <h1>button</h1>
+    </div>
+  );
+};
+
+export default Button;
---
+++ alx-movie-app/components/layouts/Layout.tsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow container mx-auto p-4">{children}</main>
+      <Footer />
+    </div>
+  );
+};
