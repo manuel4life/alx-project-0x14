c744c869acd7c64fdd8bad9a754144134eb5bbeb
manuel4life <allotey4eva@gmail.com>
2025-08-10 20:52:47 +0200

    components/layout/Footer.tsx

---
+++ alx-movie-app/components/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow container mx-auto p-4">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
